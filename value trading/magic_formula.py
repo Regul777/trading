57c6878ec74dcfe7b0db14ecd198c149f622fc10
niku2907 <36333850+niku2907@users.noreply.github.com>
2020-04-06 23:32:54 +0530

    Add magic formula in value investment

---
+++ value trading/magic_formula.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 19:41:46 2020
+
+@author: nishant.gupta
+"""
+
+# Greenblatt's Magic Formula Implementation
+# Ref: Mayank Basu's Udemy course on Algorithmic trading
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+tickers = ["AMZN", "NTPC.BO"]
+
+#list of tickers whose financial data needs to be extracted
+financial_dir = {}
+
+for ticker in tickers:
+    try:
+        #getting balance sheet data from yahoo finance for the given ticker
+        temp_dir = {}
+        url = 'https://in.finance.yahoo.com/quote/'+ticker+'/balance-sheet?p='+ticker
+        page = requests.get(url)
+        page_content = page.content
+        soup = BeautifulSoup(page_content,'html.parser')
+        tabl = soup.find_all("div", {"class" : "M(0) Whs(n) BdEnd Bdc($seperatorColor) D(itb)"})
+        for t in tabl:
+            rows = t.find_all("div", {"class" : "rw-expnded"})
+            for row in rows:
+                temp_dir[row.get_text(separator='|').split("|")[0]]=row.get_text(separator='|').split("|")[1]
+        
+        #getting income statement data from yahoo finance for the given ticker
+        url = 'https://in.finance.yahoo.com/quote/'+ticker+'/financials?p='+ticker
+        page = requests.get(url)
+        page_content = page.content
+        soup = BeautifulSoup(page_content,'html.parser')
+        tabl = soup.find_all("div", {"class" : "M(0) Whs(n) BdEnd Bdc($seperatorColor) D(itb)"})
+        for t in tabl:
+            rows = t.find_all("div", {"class" : "rw-expnded"})
+            for row in rows:
+                temp_dir[row.get_text(separator='|').split("|")[0]]=row.get_text(separator='|').split("|")[1]
+        
+        #getting cashflow statement data from yahoo finance for the given ticker
+        url = 'https://in.finance.yahoo.com/quote/'+ticker+'/cash-flow?p='+ticker
+        page = requests.get(url)
+        page_content = page.content
+        soup = BeautifulSoup(page_content,'html.parser')
+        tabl = soup.find_all("div", {"class" : "M(0) Whs(n) BdEnd Bdc($seperatorColor) D(itb)"})
+        for t in tabl:
+            rows = t.find_all("div", {"class" : "rw-expnded"})
+            for row in rows:
+                temp_dir[row.get_text(separator='|').split("|")[0]]=row.get_text(separator='|').split("|")[1]
+        
+        #getting key statistics data from yahoo finance for the given ticker
+        url = 'https://in.finance.yahoo.com/quote/'+ticker+'/key-statistics?p='+ticker
+        page = requests.get(url)
+        page_content = page.content
+        soup = BeautifulSoup(page_content,'html.parser')
+        tabl = soup.findAll("table", {"class": "W(100%) Bdcl(c)"}) # try soup.findAll("table") if this line gives error 
+        for t in tabl:
+            rows = t.find_all("tr")
+            for row in rows:
+                if len(row.get_text(separator='|').split("|")[0:2])>0:
+                    temp_dir[row.get_text(separator='|').split("|")[0]]=row.get_text(separator='|').split("|")[-1]  
+                    
+        
+        # Getting market cap from the summary data
+        url = 'https://in.finance.yahoo.com/quote/' + ticker + '?p='+ 'ticker'
+        page = requests.get(url)
+        page_content = page.content
+        soup = BeautifulSoup(page_content,'html.parser')
+        tabl = soup.findAll("td", {"class": "Ta(end) Fw(600) Lh(14px)"})
+        for t in tabl:
+            t_string = str(t)
+            pos_markt_cap_val = t_string.find("MARKET_CAP-value")
+            if (pos_markt_cap_val != -1):
+                pos_span = t_string.find("</span>", pos_markt_cap_val, len(t_string))
+                pos_data_reactid = t_string.find("data-reactid=", pos_markt_cap_val, len(t_string))
+                temp = t_string[pos_data_reactid :pos_span]
+                pos = temp.find(">")
+                val = temp[pos + 1 : len(temp)]
+                if (pos != -1):                    
+                    #print(val)
+                    temp_dir["Market cap (intra-day)"] = val
+                    break
+        #combining all extracted information with the corresponding ticker
+        financial_dir[ticker] = temp_dir
+    except:
+        print("Problem scraping data for ",ticker)
+
+
+#storing information in pandas dataframe
+combined_financials = pd.DataFrame(financial_dir)
+combined_financials.dropna(how='all',axis=1,inplace=True) #dropping columns with all NaN values
+tickers = combined_financials.columns #updating the tickers list based on only those tickers whose values were successfully extracted
+for ticker in tickers:
+    combined_financials = combined_financials[~combined_financials[ticker].str.contains("[a-z]").fillna(False)]
+
+# creating dataframe with relevant financial information for each stock using fundamental data
+stats = ["EBITDA",
+         "Depreciation & amortisation",
+         "Market cap (intra-day)",
+         "Net income available to common shareholders",
+         "Net cash provided by operating activities",
+         "Capital expenditure",
+         "Total current assets",
+         "Total current liabilities",
+         "Net property, plant and equipment",
+         "Total stockholders' equity",
+         "Long-term debt"] # change as required
+
+indx = ["EBITDA", "D&A", "MarketCap", "NetIncome", "CashFlowOps", "Capex", "CurrAsset",
+        "CurrLiab", "PPE", "BookValue", "TotDebt"]
+all_stats = {}
+for ticker in tickers:
+    try:
+        temp = combined_financials[ticker]
+        ticker_stats = []
+        for stat in stats:
+            ticker_stats.append(temp.loc[stat])
+        all_stats['{}'.format(ticker)] = ticker_stats
+    except:
+        print("can't read data for ",ticker)
+
+
+# cleansing of fundamental data imported in dataframe
+all_stats_df = pd.DataFrame(all_stats, index=indx)
+all_stats_df[tickers] = all_stats_df[tickers].replace({',': ''}, regex=True)
+all_stats_df[tickers] = all_stats_df[tickers].replace({'M': 'E+03'}, regex=True)
+all_stats_df[tickers] = all_stats_df[tickers].replace({'B': 'E+06'}, regex=True)
+all_stats_df[tickers] = all_stats_df[tickers].replace({'T': 'E+09'}, regex=True)
+all_stats_df[tickers] = all_stats_df[tickers].replace({'%': 'E-02'}, regex=True)
+
+for ticker in all_stats_df.columns:
+    all_stats_df[ticker] = pd.to_numeric(all_stats_df[ticker].values,errors='coerce')
+
+# Ignoring the stocks for whom some data is not available
+all_stats_df.dropna(axis = 1, inplace=True)
+tickers = all_stats_df.columns
+
+# calculating relevant financial metrics for each stock
+transpose_df = all_stats_df.transpose()
+final_stats_df = pd.DataFrame()
+final_stats_df["EBIT"] = transpose_df["EBITDA"] - transpose_df["D&A"]
+final_stats_df["TEV"] =  transpose_df["MarketCap"].fillna(0) \
+                         +transpose_df["TotDebt"].fillna(0) \
+                         -(transpose_df["CurrAsset"].fillna(0)-transpose_df["CurrLiab"].fillna(0))
+final_stats_df["EarningYield"] =  final_stats_df["EBIT"]/final_stats_df["TEV"]
+final_stats_df["ROC"]  = (transpose_df["EBITDA"] - transpose_df["D&A"])/(transpose_df["PPE"]+transpose_df["CurrAsset"]-transpose_df["CurrLiab"])
+
+################################Output Dataframes##############################
+
+# finding value stocks based on Magic Formula
+final_stats_val_df = final_stats_df.loc[tickers,:]
+final_stats_val_df["CombRank"] = final_stats_val_df["EarningYield"].rank(ascending=False,na_option='bottom')+final_stats_val_df["ROC"].rank(ascending=False,na_option='bottom')
+final_stats_val_df["MagicFormulaRank"] = final_stats_val_df["CombRank"].rank(method='first')
+value_stocks = final_stats_val_df.sort_values("MagicFormulaRank").iloc[:,[2, 3]]
+print("------------------------------------------------")
+print("Value stocks based on Greenblatt's Magic Formula")
+print(value_stocks)
