3263b30ed5fcb35d72388612415e423d3dc679ce
niku2907 <36333850+niku2907@users.noreply.github.com>
2020-06-01 01:43:40 +0530

    Adding stocks for value investing to V1

--- tutorials/strategy1_v1_value.py
+++ tutorials/strategy1_v1_value.py
@@ -1,42 +1,56 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Apr  5 02:28:16 2020
+Created on Mon Jun  1 01:21:43 2020
 
 @author: nishant.gupta
 """
 
-# Strategy1: This strategy used RSI + OBV slope
-# Since OBV is a leading indicator, sudden drop or rise does not mean the corresponding change
-# in the price, we are hence bouding the OBV slope inorder to avoid buy/sell during that period
-# Tuned values:
-# for BANKING sector :
-# OBV values: BUY (-30, -50) & SELL (20, 23)
-# RSI values: BUY (<50: neutral or bullish) & SELL (>55: Bullish)
-# Reason to buy even in neutral condition is based on the data for banking sector
-# For banking sector OBV slope n = 5 (last 5 days OBV slope)
-# We have used ADX in this strategy as well but that does not have much impact on the CGR
-# This is because ADX should be referred to when we are not sure about the trend but for 
-# uptrend stock OBV slope and RSI are enough and we don't need to refer to the third indicator
-# However, ADX is very useful when  a stock is constantly moving btn. Resistance and support
-# Refer to interday_strategy1.py for such a case
-# Two versions of the same strategy based on enable_using_fib_retraction
-# If enable_using_fib_retraction is used following are the parameters:
-# R1,S1 or R2,S2 or R3,S3 depends on the state the market is in right now
-# in a very bullish market R2 or R3 should be used with S1 or S2
-# in a very bearish market R1 should be used with S2 or S3
 
 from Common import DecisionParams
 from interday_testing_helper import interday_testing_helper
 from Strategy import Strategy1
 from Strategy_Runner import Strategy_Runner
 
-tickers = ["BAJFINANCE.BO",\
-           "HDFC.BO",\
-           "ICICIBANK.BO",\
-           "RELIANCE.BO",\
-           "TITAN.BO"]
-
+tickers = ["ITC.BO",\
+"UPL.BO",\
+"ONGC.BO",\
+"IOC.BO",\
+"RELIANCE.BO",\
+"NATIONALUM.BO",\
+"HINDPETRO.BO",\
+"DLF.BO",\
+"HEXAWARE.BO",\
+"NCC.BO",\
+"PHILIPCARB.BO",\
+"ARVIND.BO",\
+"BEML.BO",\
+"BALRAMCHIN.BO",\
+#"BSOFT.BO",\
+"VSTIND.BO",\
+"GSPL.BO",\
+"BIRLACORPN.BO",\
+"RAIN.BO",\
+"TV18BRDCST.BO",\
+"BALAMINES.BO",\
+"RAYMOND.BO",\
+"DBREALTY.BO",\
+"CYIENT.BO",\
+"SUDARSCHEM.BO",\
+"DIVISLAB.BO",\
+"FRETAIL.BO",\
+#"ISEC.BO",\
+"NBCC.BO",\
+"GMRINFRA.BO",\
+"JSWENERGY.BO",\
+#"CHOLAHLDNG.BO",\
+"LTI.BO",\
+"MRPL.BO",\
+"RAJESHEXPO.BO",\
+"MFSL.BO",\
+"EXIDEIND.BO",\
+"RAMCOCEM.BO",\
+"SUPREMEIND.BO"]
 
 interday_data = interday_testing_helper.get_interday_collated_data(tickers, n = 735, delta = 0)
 buy_params = DecisionParams(rsi_level = 50, min_obv_slope = 40, max_obv_slope = 90, adx = 0)
@@ -68,8 +82,9 @@ num_loss = result.num_losses
 resultant_money = result.resultant_money
 holdings_data = result.holdings_data
 holdings_data = holdings_data.sort_values("Pct hold")
-interday_testing_helper.send_mail_for_interesting_stocks(cummulative_ohlc_data, tickers, \
-                                                         holdings_data, version = "V2")
+interday_testing_helper.send_mail_for_interesting_stocks(cummulative_ohlc_data, tickers, holdings_data,\
+                                                         version = "value")
+
 latest_data = interday_testing_helper.get_latest_day_data(cummulative_ohlc_data, holdings_data, tickers)
 print("Latest Data")
 print(latest_data)
